c3533453379d0eda6fc59d9fcf9a644cb31341c7
bens <5878794@qq.com>
2015-07-14 14:15:52 +0800

    add css3animate  function

--- phone_js/lib.js
+++ phone_js/lib.js
@@ -174,53 +174,142 @@ $.fn.cssAnimate=(function(){
 
 
 
-//TODO
-$.fn.css3Animate = function(obj,time,type,infinite,alternate,callback){
-	var id = "__keyframes_"+DEVICE.counter()+"__";
-	time = parseInt(time) || 1000;
-	type = type || "linear";
-	infinite = $.getBloom(infinite);
-	callback = $.getFunction(callback);
-	alternate = $.getBloom(alternate);
+//css3 class动画
+//$.fn.css3Animate(params)
+//@param obj     {"0%":"transform:scale(1);background:#000;","100%":"transform:scale(2);background:#fff;"}
+//@param time    时间毫秒:2000
+//@param type    动画方式:linear
+//@param infinite  动画是否循环: true/false
+//@param alternate 动画是否方向:  true/false
+//@param callback  动画完成回调:fn    循环时无效
 
-	time = time+"ms";
-	infinite = (infinite)? "infinite" :"";
-	alternate = (alternate)? "alternate" : "";
-	var class_name = id+"class__";
 
-	if(!$.isObject(obj)){
-		throw("css3Animate 参数样式结构错误");
-		return;
+//停止循环的动画
+//$.fn.removeCss3Animate();
+
+
+$.fn.css3Animate = (function(){
+	var fns = {},
+		clearFn = function(obj,_id){
+			obj.get(0).removeEventListener(DEVICE.ANIEND_EV,fns[_id],false);
+			obj.removeCss3Animate();
+			delete fns[_id];
+		},
+		addFn = function(id,obj,callback){
+			var _id = "__temp_"+DEVICE.counter()+"__";
+			obj.get(0).addEventListener(DEVICE.ANIEND_EV,fns[_id] = function(e){
+				if(id == e.animationName){
+					callback.call(this);
+					clearFn(obj,_id);
+				}
+			},false);
+		};
+
+	return function(obj,time,type,infinite,alternate,callback){
+		var id = "__keyframes_"+DEVICE.counter()+"__";
+		time = parseInt(time) || 1000;
+		type = type || "linear";
+		infinite = $.getBloom(infinite);
+		//callback = $.getFunction(callback);
+		alternate = $.getBloom(alternate);
+
+		time = time+"ms";
+		infinite = (infinite)? "infinite" :"";
+		alternate = (alternate)? "alternate" : "";
+		var class_name = id+"class__";
+
+		if(!$.isObject(obj)){
+			throw("css3Animate 参数样式结构错误");
+		}
+
+
+
+		//生成style
+		var last_style = "";
+		var style = $("<style id='"+class_name+"'></style>");
+
+		var css = " animation: " + id + " " + time + " " + type + " " + infinite + " " + alternate +";";
+		css = $.css3(css);
+		css = "."+class_name+"{"+css+"} @keyframes "+id+"{";
+
+		for(var key in obj){
+			if(obj.hasOwnProperty(key)){
+				var this_val = $.css3(obj[key]);
+				css += key + " {" + this_val + "}";
+				last_style = this_val;
+			}
+		}
+
+		css +=  "}";
+
+
+		style.text(css);
+		$("head").append(style);
+
+
+
+		//生成最终的css
+		var last_css = {};
+		last_style = last_style.split(";");
+		for(var z=0,zl=last_style.length;z<zl;z++){
+			var this_style = last_style[z].split(":");
+			if(this_style.length == 2){
+				var _key = $.trim(this_style[0]),
+					_val = $.trim(this_style[1]);
+				last_css[_key] = _val;
+			}
+		}
+
+
+
+
+		$(this).each(function(){
+			if($(this).css("display") == "none" || $(this).css("visibility") == "hidden"){
+
+			}else{
+				$(this).addClass(class_name);
+				$(this).css(last_css);
+				$(this).get(0).__animate_css3_class__ = class_name;
+			}
+		});
+
+
+		if(!$.isFunction(callback)){return $(this);}
+		if(infinite){return $(this);}
+
+
+		$(this).each(function(){
+			if($(this).css("display") == "none" || $(this).css("visibility") == "hidden"){
+
+			}else{
+				addFn(id,$(this),callback);
+			}
+		});
+
+		return $(this);
 	}
-
-	var style = $("<style id='"+id+"'></style>");
-
-	var css = " animation: " + id + " " + time + " " + type + " " + infinite + " " + alternate +";";
-	css = $.css3(css);
-	css = "."+class_name+"{"+css+"} @keyframes "+id+"{";
-
-	for(var key in obj){
-		if(obj.hasOwnProperty(key)){
-			var this_val = $.css3(obj[key]);
-			css += key + " {" + this_val + "}";
+})();
 
 
+
+$.fn.removeCss3Animate = function(){
+	var temp = {};
+	$(this).each(function(){
+		var class_name = $(this).get(0).__animate_css3_class__;
+		temp[class_name] = true;
+		$(this).removeClass(class_name);
+	});
+
+	for(var key in temp){
+		if(temp.hasOwnProperty(key)){
+			if($("."+key).length == 0){
+				$("#"+key).remove();
+			}
 		}
 	}
-
-	css +=  "}";
-
-
-	style.text(css);
-	$("body").append(style);
-
-	$(this).addClass(class_name);
-
-
-
-
-
 };
+
+
 
 
 var DEVICE = {};
@@ -371,6 +460,19 @@ var DEVICE = {};
 				'Moz': 'transitionend',
 				'O': 'otransitionend',
 				'ms': 'MSTransitionEnd'
+			};
+
+			return transitionEnd[vendor];
+		})(),
+		ANIEND_EV = (function(){
+			if (vendor === false) return "animationEnd";
+
+			var transitionEnd = {
+				'': 'animationEnd',
+				'webkit': 'webkitAnimationEnd',
+				'Moz': 'mozAnimationEnd',
+				'O': 'oanimationend',
+				'ms': 'MSAnimationEnd'
 			};
 
 			return transitionEnd[vendor];
@@ -592,6 +694,7 @@ var DEVICE = {};
 	DEVICE.END_EV = END_EV;     //释放
 	DEVICE.CANCEL_EV = CANCEL_EV;      //结束
 	DEVICE.TRNEND_EV = TRNEND_EV;       //变形结束 webkitTransitionEnd
+	DEVICE.ANIEND_EV = ANIEND_EV;       //webkitAnimationEnd
 
 
 	DEVICE.nextFrame = nextFrame;
